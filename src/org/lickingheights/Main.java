5e5b4f7e7472349f01013c50ee53c950c1885d0f
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-09-26 10:10:56 -0400

    did calculations for axis of symmetry and discriminant

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -5,23 +5,24 @@ public class Main {
     public static void main(String[] args) {
 
         //todo declare variables a,b,c,discrimaniant, axis of symmetry,parabolafacing, x,y
-        double a,b,c,discrimaniant,axisOfSymmetry,x,y;
+        double a,b,c,discrimainant,axisOfSymmetry,x,y;
         String parabolaFacing;
-        a=5;
-        b=3;
-        c=1;
+
         // todo initialize manually
-        //
+         a=5;
+         b=3;
+         c=1;
         // todo do calculations
-        //
+        axisOfSymmetry = -b/(2*a);
+        discrimainant =  (b*b)-4(a*c);
         //todo print results
 
         // todo not in code check results
-        //
+
         // todo initilize scanner and redo the initialize area
-        //
+
         // todo double check results
-        //
+
         // write your code here
     }
 }
