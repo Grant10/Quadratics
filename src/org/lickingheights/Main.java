8b83b5894def8fc189573800267a8f7f62b78cc0
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-09-26 09:57:20 -0400

    declared variables

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         //todo declare variables a,b,c,discrimaniant, axis of symmetry,parabolafacing, x,y
-        //
+        double a,b,c,discrimaniant,axisOfSymmetry,x,y;
+        String parabolaFacing;
         // todo initialize manually
         //
         // todo do calculations
