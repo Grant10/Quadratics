12428949a0533e5970001a87a73b163782fdd11d
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-09-26 09:51:30 -0400

    added todo steps

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -3,6 +3,19 @@ package org.lickingheights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //todo declare variables a,b,c,discrimaniant, axis of symmetry,parabolafacing, x,y
+        //
+        // todo initialize manually
+        //
+        // todo do calculations
+        //
+        // todo not in code check results
+        //
+        // initilize scanner
+        //
+        // todo double check results
+        //
+        // write your code here
     }
 }
